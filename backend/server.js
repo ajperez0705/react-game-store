8ee1f2c51c43e69b4bc7f754671536fd169af52a
Andrew Perez <57570632+ajperez0705@users.noreply.github.com>
2021-09-22 00:35:04 -0400

    erfgre

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.text());
 
+if (process.env.NODE_ENV === "production"{
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  });
+}
+
 // Get All games
 app.get("/gamelist", async (req, res) => {
   const response = await fetch(
